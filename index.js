4db65daaf9c0dd536b06110fc3ad34953a094f3d
github username <cmyerskeitt@gmail.com>
2022-04-11 20:58:43 -0400

    final code & README

---
+++ index.js
@@ -0,0 +1,9 @@
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+    var classAvg = 0;
+    for (var i = 0; i < classPoints.length; i++){
+      classAvg += classPoints[i]; 
+    }
+    classAvg = classAvg/classPoints.length; 
+    return yourPoints > classAvg;
+}
